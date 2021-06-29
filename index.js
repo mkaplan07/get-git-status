68f7c9dd0758b3000f1195613ccbeddd1ab48b23
Mike K <michaelkaplan07@gmail.com>
2021-06-29 09:03:39 -0500

    added logic for 'no repos' & cleaned up fns

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ let startingDir = process.cwd();
 let repoCheck = (sub = '') => {
   let msgs = ['Changes not staged for commit:', 'Untracked files:', 'Changes to be committed:'];
   exec("git status", {cwd: `${startingDir}/${sub}`}, (error, stdout) => {
-    let stdoutArr = stdout.split('\n');
     if (msgs.some(msg => stdout.includes(msg))) {
+      let stdoutArr = stdout.split('\n');
       stdoutArr.forEach((line, idx, src) => {
         if (msgs.includes(line)) {
           src[idx] = `\x1b[1m${line}\x1b[0m`;
@@ -17,14 +17,15 @@ let repoCheck = (sub = '') => {
       });
       stdoutArr = stdoutArr.join('\n');
 
-      if (sub) {
-        console.log(`\x1b[32m${sub}\x1b[0m\n${stdoutArr}`);
-      } else {
-        console.log(`\x1b[32m${startingDir.split('/').pop()}\x1b[0m\n${stdoutArr}`);
-      }
+      console.log(sub ? `\x1b[32m${sub}\x1b[0m\n${stdoutArr}` : `\x1b[32m${startingDir.split('/').pop()}\x1b[0m\n${stdoutArr}`);
+      return;
     }
 
-    if (!sub && stdout.includes('up to date')) {
+    if (error) {
+      console.log(`\x1b[32m${sub}\x1b[0m is not a repo`);
+    } else if (sub && stdout.includes('up to date')) {
+      console.log(`${sub} is up to date.`);
+    } else {
       console.log(`${startingDir} is up to date.`);
     }
   });
@@ -38,21 +39,32 @@ let getStatus = () => {
     return;
   }
 
-  console.log(`Not a repo...\nScanning ${startingDir} for subdirectories...`);
+  console.log(`Not a repo\nScanning ${startingDir} for subdirectories...`);
   let results = fs.readdirSync(startingDir, { withFileTypes: true }).filter(result => result.isDirectory());
 
   if (!results.length) {
     console.log('No subdirectories. Try another folder.');
-  } else {
-    results.forEach(result => {
-      console.log(`• ${result.name}`)
-    });
-
-    console.log(`\nChecking for unstaged, untracked or uncommitted...`);
-    results.forEach(result => {
-      repoCheck(result.name); // TODO: docs/ – if repoCheck has nothing to say, 'no repos'
-    });
+    return;
   }
+
+  results.forEach(result => {
+    repoCheck(result.name);
+  });
+
+  // console.log(`\nChecking for unstaged, untracked or uncommitted...`);
+  // if (!results.length) {
+  //   console.log('No subdirectories. Try another folder.');
+  // } else {
+  //   results.forEach(result => {
+  //     console.log(`• ${result.name}`)
+  //   });
+  //
+  //   console.log(`\nChecking for unstaged, untracked or uncommitted...`);
+  //
+  //   results.forEach(result => {
+  //     repoCheck(result.name);
+  //   });
+  // }
 }
 
 getStatus();
