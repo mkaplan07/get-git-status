4a0931ee1cee987ada9882d8d523be669373be19
Mike K <michaelkaplan07@gmail.com>
2021-07-01 18:53:42 -0500

    fixed formatting for 'Your branch is ahead'

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const { exec } = require('child_process');
 let startingDir = process.cwd();
 
 let repoCheck = (sub = '') => {
-  let msgs = ['Changes not staged', 'Untracked files:', 'Changes to be committed:', 'Your branch is ahead'];
+  let msgs = ['Changes not staged for commit:', 'Untracked files:', 'Changes to be committed:', 'Your branch is ahead'];
   exec("git status", {cwd: `${startingDir}/${sub}`}, (error, stdout) => {
     if (msgs.some(msg => stdout.includes(msg))) {
       let stdoutArr = stdout.split('\n');
       stdoutArr.forEach((line, idx, src) => {
-        if (msgs.includes(line)) {
+        if (msgs.includes(line)) { // 'Your branch is ahead' will not be underlined
           src[idx] = `\x1b[4m${line}\x1b[0m`;
         }
       });
@@ -20,9 +20,6 @@ let repoCheck = (sub = '') => {
       console.log(sub ? `• \x1b[1m${sub}\x1b[0m\n${stdoutArr}` : `${stdoutArr}`);
       return;
     }
-
-    // TODO: when “Your branch is ahead of 'origin/master'...”
-    // I don't want to bold it; just be sure the directory prints properly
 
     if (error) {
       console.log(`• ${sub} is not a repo`);
